084c69b5dde340fd591e17b2a5a14b17b070a65d
sarvesh sawant <name.sarveshsawant@gmail.com>
2023-08-12 00:01:43 +0530

    multiple task storing

--- Todo.java
+++ Todo.java
@@ -1,17 +1,47 @@
 public class Todo {
+    // global variable
+    // static means that variable common value in all object
+    static int currentTaskNumber = 1;
+    static char[][] task = new char[5][];
+
     // Java code start with main method
-    public static void main(String[] args){
+    public static void main(String[] args) {
         // printing hello to java
         System.out.println("Hello Java World !!");
 
         // define task
-        int taskNumber = 1;
-        char dot = '.';
-        
-        char[] task = {'L','e','a','r','n',' ','J','a','v','a'};
-        System.out.print(taskNumber);
-        System.out.print(dot);
-        System.out.print(' ');
-        System.out.print(task);
+        char[] task1 = { 'L', 'e', 'a', 'r', 'n', ' ', 'J', 'a', 'v', 'a' };
+        setTask(task1); //calling method with parameter
+        char[] task2 = { 'S', 't', 'a', 'r', 't', ' ', 'C', 'o', 'd', 'i', 'n', 'g' };
+        setTask(task2); 
+        showTask(); //calling method
+    }
+
+    // define funtion for using multiple set task
+    public static void setTask(char[] taskGiven) {
+        task[currentTaskNumber-1] = taskGiven;
+        currentTaskNumber++; // increamenting number by 1 for next task
+    }
+
+    // define funtion for show multiple current task
+    public static void showTask() {
+        currentTaskNumber--; // decrementing by 1 for previous
+
+        //checking currentTaskNumber is greater than zero : if yes then go inside if , else go inside else
+        if (currentTaskNumber > 0) {
+            //if block
+            char dot = '.';
+
+            System.out.print(currentTaskNumber);
+            System.out.print(dot);
+            System.out.print(' ');
+            System.out.print(task[currentTaskNumber-1]);
+            System.out.print('\n'); //course move new line
+            //recursively calls show method for display all tasks
+            showTask();
+        } else {
+            //else block
+            // stop here calls
+        }
     }
 }
