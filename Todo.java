850650d2777209b3403b73518acc2fa0b7cb384f
sarvesh sawant <name.sarveshsawant@gmail.com>
2023-08-11 21:57:24 +0530

    Storing first task

--- Todo.java
+++ Todo.java
@@ -3,5 +3,15 @@ public class Todo {
     public static void main(String[] args){
         // printing hello to java
         System.out.println("Hello Java World !!");
+
+        // define task
+        int taskNumber = 1;
+        char dot = '.';
+        
+        char[] task = {'L','e','a','r','n',' ','J','a','v','a'};
+        System.out.print(taskNumber);
+        System.out.print(dot);
+        System.out.print(' ');
+        System.out.print(task);
     }
 }
