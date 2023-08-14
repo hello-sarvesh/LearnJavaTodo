a03ebcfdf490dfe7b4b55d89901303a409c51db8
Sarvesh Sawant <name.sarveshsawant@gmail.com>
2023-08-14 17:12:53 +0530

    handle exceptions using try catch and recrecursively calling input

--- Todo.java
+++ Todo.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader; // import BufferedReader class from Java Core Libraries
 import java.io.IOException;
 import java.io.InputStreamReader; // import InputStreamReader class from Java Core Libraries
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner; // import Scanner class from Java Core Libraries
 
 public class Todo {
@@ -40,41 +42,65 @@ public class Todo {
     }
 
     public static void addNewTask() {
+        int taskNumber = 0;
         System.out.println("How many task you do ? ");
-        int taskNumber = s.nextInt();
+        try {
+            taskNumber = s.nextInt();
+            if (taskNumber < 0) {
+                System.out.println("task value must be greater than 0.");
+            }
+        } catch (Exception e) {
+            System.out.println("Invalid input. Please enter a valid task numbers value.");
+            s.nextLine();
+        }
 
         // for loop for create multiple base on taskNumber
         for (int i = 0; i < taskNumber; i++) {
             System.out.println("What is your " + (i + 1) + " task ?");
-            // user define task
-            char[] task = s.next().toCharArray();
-            setTask(task); // calling method with parameter
+            try {
+                // user define task
+                char[] task = s.next().toCharArray();
+                setTask(task); // calling method with parameter
+            } catch (Exception e) {
+                System.out.println("Invalid input. Please enter a valid task.");
+                s.nextLine();
+            }
         }
         dashboard();
     }
 
     public static void dashboard() {
-        System.out.println("_______________DashBoard Todo_______________");
-        System.out.println("1. Add New Task");
-        System.out.println("2. Show All Task");
-        System.out.println("0. exit");
-        int option = s.nextInt();
-        // switch case same as nested if else-if else
-        switch (option) {
-            case 1:
-                addNewTask();
-                break;
-            case 2:
-                showTask();
-                break;
-            case 0:
-                System.exit(0);
-                break;
-            default:
-                System.out.println("You enter wrong option .");
-                System.exit(0);
-                break;
+        String timeStamp = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").format(new Date());
+        // while loop recursively display dashbord if any input error
+        while (true) {
+            System.out.println("_______________DashBoard Todo_______________");
+            System.out.println("Date Time: " + timeStamp);
+            System.out.println("1. Add New Task");
+            System.out.println("2. Show All Task");
+            System.out.println("0. exit");
+            try {
+                int option = s.nextInt();
+                // switch case same as nested if else-if else
+                switch (option) {
+                    case 1:
+                        addNewTask();
+                        break;
+                    case 2:
+                        showTask();
+                        break;
+                    case 0:
+                        System.exit(0);
+                        break;
+                    default:
+                        System.out.println("You enter wrong option .");
+                        System.exit(0);
+                        break;
 
+                }
+            } catch (Exception e) {
+                System.out.println("Invalid input. Please enter a valid option.");
+                s.nextLine();
+            }
         }
     }
 }
