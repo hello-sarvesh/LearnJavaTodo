27cabfe299b5309cc4465e9cd8ec6005b3cf603b
sarvesh sawant <name.sarveshsawant@gmail.com>
2023-08-17 22:27:49 +0530

    remove task inside link list

--- Todo.java
+++ Todo.java
@@ -36,45 +36,99 @@ public class Todo {
         if (firstTask != null) {
             Task task = firstTask;
             System.out.print("---Your Total " + task.getTaskCount() + " Task Is---\n");
-            int i = 0;
+            int i = 1;
             while (task != null) {// while last task is not null
                 System.out.println("" + i + ". " + task.getTaskName());
                 task = task.getNextTask();
                 i++;
             }
-            ;
+            
         } else {
             System.out.println("---You Not Create Any Task---");
         }
-        dashboard();
+
     }
 
     public static void addNewTask() {
         int taskNumber = 0;
-        System.out.println("How many task you do ? ");
-        try {
-            taskNumber = s.nextInt();
-            if (taskNumber < 0) {
-                System.out.println("task value must be greater than 0.");
-            }
-        } catch (Exception e) {
-            System.out.println("Invalid input. Please enter a valid task numbers value.");
-            s.nextLine();
-        }
-
-        // for loop for create multiple base on taskNumber
-        for (int i = 0; i < taskNumber; i++) {
-            System.out.println("What is your " + (i + 1) + " task ?");
+        while (true) {
+            System.out.println("How many task you do ? ");
             try {
-                // user define task
-                String taskName = s.next();
-                setTask(taskName); // calling method with parameter
+                taskNumber = s.nextInt();
+                if (taskNumber < 0) {
+                    System.out.println("task value must be greater than 0.");
+                }
+                break;
             } catch (Exception e) {
-                System.out.println("Invalid input. Please enter a valid task.");
+                System.out.println("Invalid input. Please enter a valid task numbers value.");
                 s.nextLine();
             }
         }
-        dashboard();
+        // for loop for create multiple base on taskNumber
+        while (true) {
+            for (int i = 0; i < taskNumber; i++) {
+                System.out.println("What is your " + (i + 1) + " task ?");
+                try {
+                    // user define task
+                    String taskName = s.next();
+                    setTask(taskName); // calling method with parameter
+                } catch (Exception e) {
+                    System.out.println("Invalid input. Please enter a valid task.");
+                    s.nextLine();
+                }
+            }
+            break;
+        }
+    }
+
+    public static void removeTask() {
+        if (firstTask != null) {
+            showTask(); // first show all task
+            while (true) {
+                try {
+                    System.out.println("Enter your task index to remove : ");
+                    int index = s.nextInt();
+                    Task task = firstTask;
+                    Task prTask = null;
+                    int i = 0;
+                    while (task != null) {// while last task is not null
+                        if (i == index - 1) {
+                            System.out.println("" + index + ". " + task.getTaskName());
+                            System.out.println("You want to remove this task ? y/n : ");
+                            while (true) {
+                                try {
+                                    char remove = s.next().charAt(0);
+                                    if (remove == 'Y' || remove == 'y') {
+                                        if (prTask != null) {
+                                            // if previous is not null then point prenext to currentnext
+                                            prTask.setNextTask(task.getNextTask());
+                                        } else {
+                                            firstTask = task.getNextTask();
+                                        }
+                                    }
+                                    break;
+                                } catch (Exception e) {
+                                    // TODO: handle exception
+                                    e.printStackTrace();
+                                }
+
+                            }
+                            break;
+                        }
+                        prTask = task; // storing prevoius task
+                        task = task.getNextTask();
+                        i++;
+                    }
+                    break;
+                } catch (Exception e) {
+                    // TODO: handle exception
+                    e.printStackTrace();
+                }
+            }
+        } else {
+            System.out.println("---You Not Create Any Task---");
+
+        }
     }
 
     public static void dashboard() {
@@ -85,6 +139,7 @@ public class Todo {
             System.out.println("Date Time: " + timeStamp);
             System.out.println("1. Add New Task");
             System.out.println("2. Show All Task");
+            System.out.println("3. Remove Task");
             System.out.println("0. exit");
             try {
                 int option = s.nextInt();
@@ -92,9 +147,15 @@ public class Todo {
                 switch (option) {
                     case 1:
                         addNewTask();
+                        dashboard();
                         break;
                     case 2:
                         showTask();
+                        dashboard();
+                        break;
+                    case 3:
+                        removeTask();
+                        dashboard();
                         break;
                     case 0:
                         System.exit(0);
