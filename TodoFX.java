24804005718c90b4be3c7b9173a07489de29533e
sarvesh sawant <name.sarveshsawant@gmail.com>
2023-08-22 22:36:56 +0530

    small change

--- TodoFX.java
+++ TodoFX.java
@@ -2,8 +2,10 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
@@ -13,20 +15,30 @@ public class TodoFX extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         // Create a scene and place a button in the scene
-        VBox root = new VBox(); // layout
+        GridPane root = new GridPane(); // layout
+        
+        root.setHgap(10);
+        root.setVgap(10);
+        root.setAlignment(Pos.CENTER);
+
         Text txtHelloJava = new Text();
         txtHelloJava.setText("Hello Java World !!");
-        root.getChildren().addAll(txtHelloJava);
+        root.add(txtHelloJava,0,1);
 
         String timeStamp = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").format(new Date());
         Text txtTitle = new Text("_______________DashBoard Todo_______________");
         Text txtDateTime = new Text("Date Time: " + timeStamp);
-        Text txtOne = new Text("1. Add New Task");
-        Text txtTwo = new Text("2. Show All Task");
-        Text txtThree = new Text("3. Remove Task");
-        Text txtZero = new Text("0. exit");
-        root.getChildren().addAll(txtTitle,txtDateTime,txtOne,txtTwo,txtThree,txtZero);
-        
+        Button btnOne = new Button("1. Add New Task");
+        Button btnTwo = new Button("2. Show All Task");
+        Button btnThree = new Button("3. Remove Task");
+        Button btnZero = new Button("0. exit");
+        root.add(txtTitle,0,2);
+        root.add(txtDateTime,0,3);
+        root.add(btnOne,0,4);
+        root.add(btnTwo,0,5);
+        root.add(btnThree,0,6);
+        root.add(btnZero,0,7);
+
         Scene scene = new Scene(root, 200, 250);
 
         primaryStage.setTitle("MyJavaFX"); // Set the stage title
